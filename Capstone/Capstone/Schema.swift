1d599a59063fd5b5aba9cd799b1e8c7eb1fed4ac
Devon Reing <devon.reing@yahoo.com>
2024-10-07 20:36:57 -0400

    schema updates

--- Capstone/Capstone/Schema.swift
+++ Capstone/Capstone/Schema.swift
@@ -21,7 +21,7 @@ class Children: Object {
 }
 
 // relationship
-class PickUp: Object {
+class PickUp {
     @Persisted var cid: Children // foreign key declaration
     @Persisted var email: User
     @Persisted var validDates: Date
@@ -35,21 +35,104 @@ class RideRequest: Object {
 }
 
 // relationship
-class Send: Object {
+class Send {
     @Persisted var emailSend: User // person sending request
     @Persisted var emailReceive: User // person receiving request
 }
 
 // relationship
-class Receive: Object {
+class Receive {
     @Persisted var emailSend: User // person sending request
     @Persisted var emailReceive: User // person receiving request
 }
 
 // relationship
-class For: Object {
+class For {
     @Persisted var rrid: RideRequest
     @Persisted var cid: List<Children> // list allows for ride request to be for multiple children or one child
 }
 
-//
+class Teachers: Object {
+    @Persisted(primaryKey: true) var tid: Int
+    @Persisted var name: String
+    @Persisted var classroom: String // incase buildings are involved or letters such as 3A
+    @Persisted var email: String
+    @Persisted var phone: String // incase of country codes, (), and dashes
+}
+
+// relationship
+class Teach {
+    @Persisted var cid: Children
+    @Persisted var tid: Teachers
+}
+
+class School: Object {
+    @Persisted(primaryKey: true) var sid: Int
+    @Persisted var name: String
+    @Persisted var address: String
+    @Persisted var city: String
+    @Persisted var state: String
+}
+
+// relationship
+class Contain {
+    @Persisted var email: User
+    @Persisted var sid: School
+}
+
+// relationship
+class Employ {
+    @Persisted var sid: School
+    @Persisted var tid: Teachers
+}
+
+class SchoolCalendar: Object {
+    @Persisted(primaryKey: true) var calid: Int
+    @Persisted var date: Date
+}
+
+// relationship
+class Update {
+    @Persisted var tid: Teachers
+    @Persisted var calid: SchoolCalendar
+}
+
+// relationship
+class Create {
+    @Persisted var sid: School
+    @Persisted var calid: SchoolCalendar
+}
+
+// relationship
+class AddTo {
+    @Persisted var email: User
+    @Persisted var calid: SchoolCalendar
+}
+
+class CalEvents: Object {
+    @Persisted(primaryKey: true) var eid: Int
+    @Persisted var title: String
+    @Persisted var message: String
+    @Persisted var audience: String // may change this ??
+    @Persisted var date: Date
+}
+
+// relationship
+class Have {
+    @Persisted var eid: CalEvents
+    @Persisted var calid: SchoolCalendar
+}
+
+class Alerts: Object {
+    @Persisted(primaryKey: true) var aid: Int
+    @Persisted var priority: Int
+    @Persisted var title: String
+    @Persisted var body: String
+    @Persisted var audience: String // may change this ??
+}
+
+// relationship
+class AlertSend {
+    @Persisted var calid: SchoolCalendar
+    @Persisted var aid: Alerts
+}
